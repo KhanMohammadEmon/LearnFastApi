956f1fb5b97093ff7852aae2c24eba8575c65007
KHANMOHAMMADEMON <kemon201394@bscse.uiu.ac.bd>
2025-04-09 16:03:48 +0600

    Finished the uvicorn setup

--- uvicorn_web_server/app.py
+++ uvicorn_web_server/app.py
@@ -1,1 +1,19 @@
-print("Hello")
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+
+    try:
+        await send({
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [(
+                b"content-type", b"text-plain"
+            )]
+        })
+
+        await send({
+            "type": "http.response.body",
+            "body": b"<h1>This is Emon. How are you</h1>"
+        })
+
+    except:
+        print("Fail to start server!")
